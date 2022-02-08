6b7a974bfdd1c7a7785c18de8b279aecd548ac12
Elias-nb <enacerbey@gmail.om>
2022-02-08 10:52:06 +0100

    test

--- tas-de-sable.py
+++ tas-de-sable.py
@@ -1,6 +1,6 @@
 #########################################
 # groupe MITD02
-# Elias NACER-BEY
+# Elias NACER-BEY o
 # Hans VEERABADREN
 # Baptiste PARIS
 # https://github.com/Elias-nb/Projet-tas-de-sable/
